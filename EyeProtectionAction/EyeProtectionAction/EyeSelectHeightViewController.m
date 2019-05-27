72a612c4b2aa8feb28573c8568b38506a716aeea
秦晓康 <qinxiaokang@ppdai.com>
2019-05-27 20:39:59 +0800

    【需求】【1.0.0】【#】pickerView 开发中

--- EyeProtectionAction/EyeProtectionAction/EyeSelectHeightViewController.m
+++ EyeProtectionAction/EyeProtectionAction/EyeSelectHeightViewController.m
@@ -8,26 +8,55 @@
 
 #import "EyeSelectHeightViewController.h"
 
-@interface EyeSelectHeightViewController ()
+@interface EyeSelectHeightViewController ()<UIPickerViewDelegate,UIPickerViewDataSource>
 @property (weak, nonatomic) IBOutlet UIPickerView *selectHeightPickView;
-
+@property (weak, nonatomic) IBOutlet UILabel *titleL;
+@property (nonatomic, strong) NSMutableArray *dataArr;
 @end
 
 @implementation EyeSelectHeightViewController
 
+- (NSMutableArray *)dataArr
+{
+    if (!_dataArr) {
+        _dataArr = [NSMutableArray array];
+    }
+    return _dataArr;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    for (NSInteger i = 80; i<= 220; i++) {
+        [self.dataArr addObject:[NSString stringWithFormat:@"%ldcm",i]];
+    }
+    [self.selectHeightPickView selectRow:98 inComponent:0 animated:YES];
+    [self pickerView:self.selectHeightPickView didSelectRow:98 inComponent:0];
+
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)numberOfComponentsInPickerView:(nonnull UIPickerView *)pickerView {
+    return 1;
 }
-*/
+
+- (NSInteger)pickerView:(nonnull UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.dataArr.count;
+}
+
+//列显示的数据
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger) row forComponent:(NSInteger)component {
+    return self.dataArr[row];
+}
+
+#pragma mark - delegate
+// 选中某一组的某一行
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    self.titleL.text = self.dataArr[row];
+}
+
+
+
+
 
 @end
