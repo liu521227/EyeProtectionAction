4856b965cdf15ddc37b5579457082823be427251
秦晓康 <qinxiaokang@ppdai.com>
2019-06-03 21:51:42 +0800

    开发中

---
+++ EyeProtectionAction/EyeProtectionAction/EyeStartTestViewController.h
@@ -0,0 +1,17 @@
+//
+//  EyeStartTestViewController.h
+//  EyeProtectionAction
+//
+//  Created by 秦晓康 on 2019/6/3.
+//  Copyright © 2019 qxk. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EyeStartTestViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EyeProtectionAction/EyeProtectionAction/EyeStartTestViewController.m
@@ -0,0 +1,32 @@
+//
+//  EyeStartTestViewController.m
+//  EyeProtectionAction
+//
+//  Created by 秦晓康 on 2019/6/3.
+//  Copyright © 2019 qxk. All rights reserved.
+//
+
+#import "EyeStartTestViewController.h"
+
+@interface EyeStartTestViewController ()
+
+@end
+
+@implementation EyeStartTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
