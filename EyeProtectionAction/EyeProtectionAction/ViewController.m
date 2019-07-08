8759658740b324df4f8a2e1489e39b39d760edbd
秦晓康 <qinxiaokang@ppdai.com>
2019-07-08 23:45:11 +0800

    接口调用基本完成

--- EyeProtectionAction/EyeProtectionAction/ViewController.m
+++ EyeProtectionAction/EyeProtectionAction/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "EyeTool.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface ViewController ()
 
@@ -22,6 +23,18 @@
     [[EyeTool sharedSingleton].testResultDic removeAllObjects];
     [EyeTool sharedSingleton].isLeftEye = NO;
     [EyeTool sharedSingleton].isDecimal = NO;
+    //创建数据请求管理对象
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", @"text/xml", @"text/plain",@"application/xml", nil];
+    [manager GET:@"http://topicscreated.com/UserServiceTools.json" parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"%@",responseObject);
+        NSLog(@"address = %@",responseObject[@"address"]);
+
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@",error.userInfo);
+    }];
 }
 
 - (void)viewDidLoad {
